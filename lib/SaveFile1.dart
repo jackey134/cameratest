78a66e50fcf8268c5c0601e116eaf27ba2076a8d
jackey134 <liu8825515@gmail.com>
2022-11-11 22:57:16 +0800

    master

--- lib/SaveFile1.dart
+++ lib/SaveFile1.dart
@@ -30,8 +30,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final Dio dio = Dio();
-  bool loading = false;
   double progress = 0;
 
   Future<bool> saveVideo(String url, String fileName) async {
@@ -73,10 +71,10 @@ class _MyHomePageState extends State<MyHomePage> {
       if (await directory.exists()) {
         await dio.download(url, saveFile.path,
             onReceiveProgress: (value1, value2) {
-              setState(() {
-                progress = value1 / value2;
-              });
-            });
+          setState(() {
+            progress = value1 / value2;
+          });
+        });
         if (Platform.isIOS) {
           await ImageGallerySaver.saveFile(saveFile.path,
               isReturnPathOfIOS: true);
@@ -92,60 +90,21 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<bool> _requestPermission(Permission permission) async {
     if (await permission.isGranted) {
+      //確認是否通過權限，發起權限申請
       return true;
     } else {
-      var result = await permission.request();
+      var result = await permission.request(); //若為否回傳"要求權限"
       if (result == PermissionStatus.granted) {
+        //判斷現在權限是否通過
         return true;
+      } else {
+        return false;
       }
     }
-    return false;
-  }
-
-  downloadFile() async {
-    setState(() {
-      loading = true;
-      progress = 0;
-    });
-    bool downloaded = await saveVideo(
-        "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4",
-        "video.mp4");
-    if (downloaded) {
-      print("File Downloaded");
-    } else {
-      print("Problem Downloading File");
-    }
-    setState(() {
-      loading = false;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: loading
-            ? Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: LinearProgressIndicator(
-            minHeight: 10,
-            value: progress,
-          ),
-        )
-            : TextButton.icon(
-            icon: Icon(
-              Icons.download_rounded,
-              color: Colors.grey,
-            ),
-            style: TextButton.styleFrom(
-                primary:Colors.black),
-            onPressed: downloadFile,
-            //padding: const EdgeInsets.all(10),
-            label: Text(
-              "Download Video",
-              style: TextStyle(color: Colors.black, fontSize: 25),
-            )),
-      ),
-    );
+    return Scaffold();
   }
 }
